e75b0f793a97186c0da14adcaf70fc61b6d0322c
Michael Rash <mrash@MyMBP.local>
2019-02-22 00:11:58 -0600

    Fixed the marked watched to call the right server

--- plexsync.py
+++ plexsync.py
@@ -2,9 +2,7 @@
 
 import getpass
 from plexapi.myplex import MyPlexAccount
-from multiprocessing.dummy import Pool as ThreadPool
-
-pool = ThreadPool(4)
+import multiprocessing.dummy
 username = input('Username: ')
 password = getpass.getpass(prompt ='Password: ')
 account = MyPlexAccount(username, password)
@@ -21,91 +19,56 @@ allepisodess1 = 0
 allepisodess2 = 0
 allshows = 0
 
-#server_1_shows = set(list(map((lambda x: x.title), conn_1.library.section('TV Shows').search())))
-#server_2_shows = set(list(map((lambda x: x.title), conn_2.library.section('TV Shows').search())))
+server_1_shows = set(list(map((lambda x: x.title), conn_1.library.section('TV Shows').search())))
+server_2_shows = set(list(map((lambda x: x.title), conn_2.library.section('TV Shows').search())))
 
-#common_shows = server_1_shows & server_2_shows
-common_shows = ['Ash vs Evil Dead']
-#common_shows = ['Ash vs Evil Dead','Disenchantment', 'Justified', 'This Is Us', "Marvel's Iron Fist"]
+common_shows = server_1_shows & server_2_shows
 
-
-def getwatched(watched):
-    serverdict = {'s1':{}, 's2': {}}
-    s1set = set()
-    s2set = set()
-    for ep in conn_1.library.section('TV Shows').get(watched).episodes():
-        if ep.isWatched == True:
-            strep = str(ep).split(':')[2]
-            s1set.add(strep)
-            serverdict['s1'][strep] = ep.title
-        else:
-            continue
-
-    for ep2 in conn_2.library.section('TV Shows').get(watched).episodes():
-        if ep2.isWatched == True:
-            strep2 = str(ep2).split(':')[2]
-            s2set.add(strep2)
-            serverdict['s2'][strep] = ep2.title
-            strep = str(ep2)
-            serverdict['s2'][strep] = ep2.title
-        else:
-            continue
-    set1diff = s1set.difference(s2set)
-    set2diff = s2set.difference(s1set)
-    print(len(set1diff))
-    print(len(set2diff))
-    print(set1diff)
-    for i in serverdict['s1'][set1diff]:
-        print(i)
-
-
-def getepisodes(shownames):
+def getwatched(shows):
     global allshows
     global allepisodess1
     global allepisodess2
+    serverdict = {'s1':{}, 's2': {}}
     s1set = set()
     s2set = set()
     allshows += 1
-    for ep in conn_1.library.section('TV Shows').get(shownames).episodes():
+    for ep in conn_1.library.section('TV Shows').get(shows).episodes():
         allepisodess1 += 1
+        strep = str(ep).split(':')[2]
+        serverdict['s1'][strep] = ep.title
         if ep.isWatched == True:
-            s1set.add(ep.title)
+            s1set.add(strep)
         else:
             continue
-    for ep2 in conn_2.library.section('TV Shows').get(shownames).episodes():
+    for ep2 in conn_2.library.section('TV Shows').get(shows).episodes():
         allepisodess2 += 1
+        strep2 = str(ep2).split(':')[2]
+        serverdict['s2'][strep2] = ep2.title
         if ep2.isWatched == True:
-            s2set.add(ep2.title)
+            s2set.add(strep2)
         else:
             continue
-    #print(s1set)
-    #print(s2set)
     set1diff = s1set.difference(s2set)
     set2diff = s2set.difference(s1set)
-    #print(set1diff)
-    #print(set2diff)
-    i = 0
-    p = 0
-    for s1show in set1diff:
-        try:
-            conn_2.library.section('TV Shows').get(shownames).get(s1show).markWatched()
-            i += 1
-        except:
-            print('The show does not exist on both servers', shownames,"-",s1show)
-            continue
-    for s2show in set2diff:
-        try:
-            conn_1.library.section('TV Shows').get(shownames).get(s2show).markWatched()
-            p += 1
-        except:
-            print('The show does not exist on both servers', shownames,"-",s2show)
-            continue
+    s1 = 0
+    s2 = 0
+    if len(set1diff) > 0:
+        for sdiff1 in set1diff:
+            conn_2.library.section('TV Shows').get(shows).get(serverdict['s2'][sdiff1]).markWatched()
+            s2 += 1
+    else:
+        pass
+    if len(set2diff) > 0:
+        for sdiff2 in set2diff:
+            conn_1.library.section('TV Shows').get(shows).get(serverdict['s1'][sdiff2]).markWatched()
+            s1 += 1
+    else:
+        pass
+    print("Marked", s1 ,"episodes of",shows,"watched on",server_1_name)
+    print("Marked", s2 ,"episodes of",shows,"watched on",server_2_name)
 
-    print("Marked", p ,"episodes of",shownames,"watched on",server_1_name)
-    print("Marked", i ,"episodes of",shownames,"watched on",server_2_name)
-
-pool.map(getwatched, common_shows)
-#pool.map(getepisodes, common_shows)
-pool.close()
-#pool.close()
-#print('Checked ',allshows, 'shows with',allepisodess1,'episodes on',server_1_name,'and',allepisodess2,'episodes on',server_2_name)
+if __name__ == "__main__":
+    pool = multiprocessing.dummy.Pool(10)
+    pool.map(getwatched, common_shows)
+    pool.close()
+print('Checked ',allshows, 'shows with',allepisodess1,'episodes on',server_1_name,'and',allepisodess2,'episodes on',server_2_name)
